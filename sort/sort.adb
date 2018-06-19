ff856fceb7c0944d797206ead7f7b308d12fddb3
wiekonek <wiekonek@gmail.com>
2018-06-19 11:19:24 +0200

    Add .editorconfig and reformat files

--- sort/sort.adb
+++ sort/sort.adb
@@ -3,40 +3,40 @@ use Ada.Text_IO;
 
 
 procedure Sort is
-	type Vector is array (Integer range <>) of Float;
-	arr : Vector(1..10) := (0.31, 0.9, 0.8, 0.4, 0.3, 1.0, 0.7, 0.2, 0.45, 0.246); 
+    type Vector is array (Integer range <>) of Float;
+    arr : Vector(1..10) := (0.31, 0.9, 0.8, 0.4, 0.3, 1.0, 0.7, 0.2, 0.45, 0.246);
 
-	procedure Sort_Vector(tab : in out Vector) is
-		procedure Swap(Left, Right: in out Float) is
-			Temp : Float := Left;
-		begin
-			Left := Right;
-			Right := Temp;
-		end Swap;
-	begin
-		for j in tab'range loop
-			for i in tab'first..tab'last-j loop
-				if tab(i) > tab(i+1) then
-					swap(tab(i), tab(i+1));
-				end if;
-			end loop;
-		end loop;
-	end Sort_Vector;
+    procedure Sort_Vector(tab : in out Vector) is
+        procedure Swap(Left, Right: in out Float) is
+            Temp : Float := Left;
+        begin
+            Left := Right;
+            Right := Temp;
+        end Swap;
+    begin
+        for j in tab'range loop
+            for i in tab'first..tab'last-j loop
+                if tab(i) > tab(i+1) then
+                    swap(tab(i), tab(i+1));
+                end if;
+            end loop;
+        end loop;
+    end Sort_Vector;
 begin
-	Put_Line("Before:");
-	for i in arr'range loop
-		Put(Float'Image(arr(i)));
-		Put(" ");
-	end loop;
-	New_Line;
+    Put_Line("Before:");
+    for i in arr'range loop
+        Put(Float'Image(arr(i)));
+        Put(" ");
+    end loop;
+    New_Line;
 
-	Sort_Vector(arr);
-	
-	Put_Line("After:");	
-	for i in arr'range loop
-		Put(Float'Image(arr(i)));
-		Put(" ");
-	end loop;
-	New_Line;
+    Sort_Vector(arr);
+
+    Put_Line("After:");
+    for i in arr'range loop
+        Put(Float'Image(arr(i)));
+        Put(" ");
+    end loop;
+    New_Line;
 end;
 
--- tasks/tasks.adb
+++ tasks/tasks.adb
@@ -5,53 +5,53 @@ use Ada.Text_IO;
 
 procedure Tasks is
 
-	task producer;
-	task consumer;
-	task type BUF is
-		entry Put (param: in Integer);
-		entry Get (param: out Integer);
-	end BUF;
-	bufTask: BUF;
-	productId: Integer := 0;
+    task producer;
+    task consumer;
+    task type BUF is
+        entry Put (param: in Integer);
+        entry Get (param: out Integer);
+    end BUF;
+    bufTask: BUF;
+    productId: Integer := 0;
 
-	task body producer is
-	begin
-		loop
-			delay 1.0;
-			bufTask.Put(productId);
-			Put("Producing: ");
-			Put_Line(Integer'Image(productId));
-			productId := productId + 1;
-		end loop;
-	end;
+    task body producer is
+    begin
+        loop
+            delay 1.0;
+            bufTask.Put(productId);
+            Put("Producing: ");
+            Put_Line(Integer'Image(productId));
+            productId := productId + 1;
+        end loop;
+    end;
 
-	task body consumer is
-		product: Integer;
-	begin
-		loop
-			bufTask.Get(product);
-			Put("Consuming: ");
-			Put_Line(Integer'Image(product));
-		end loop;
-	end;
+    task body consumer is
+        product: Integer;
+    begin
+        loop
+            bufTask.Get(product);
+            Put("Consuming: ");
+            Put_Line(Integer'Image(product));
+        end loop;
+    end;
 
-	task body BUF is
-		buffer: Integer := -1; -- -1 = buffer	       
-	begin
-		loop
-			select
-				when buffer = -1 => accept Put(param: in Integer) do
-					buffer := param;
-				end Put;
-			or
-				when buffer > -1 => accept Get(param: out Integer) do
-					param := buffer;
-					buffer := -1;
-				end Get;
-			end select;
-		end loop;
-	end;
+    task body BUF is
+        buffer: Integer := -1; -- -1 = buffer
+    begin
+        loop
+            select
+                when buffer = -1 => accept Put(param: in Integer) do
+                    buffer := param;
+                end Put;
+            or
+                when buffer > -1 => accept Get(param: out Integer) do
+                    param := buffer;
+                    buffer := -1;
+                end Get;
+            end select;
+        end loop;
+    end;
 begin
-	null;
+    null;
 end Tasks;
 
